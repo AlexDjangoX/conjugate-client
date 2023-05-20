8f23acc210ade48e54eb0e8fa5d20192cfa609e4
AlexDjangoX <alexmonk17@gmail.com>
2023-05-20 15:10:11 +0200

    changed video

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,17 +9,17 @@ const Hero = () => {
 
   return (
     <>
-      <div className='hero-container'>
+      <div className="hero-container">
         <h1>Polish Language Ninja</h1>
 
-        <div className='video'>
+        <div className="video">
           {youTubeUrl && (
             <>
               <div style={{ padding: '75% 0 0 0', position: 'relative' }}>
                 <iframe
-                  src='https://player.vimeo.com/video/786325458?h=f713208f76&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'
-                  frameBorder='0'
-                  allow='autoplay; fullscreen; picture-in-picture'
+                  src="https://player.vimeo.com/video/824510875?h=3e9c4120e9&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                  frameBorder="0"
+                  allow="autoplay; fullscreen; picture-in-picture"
                   allowFullScreen
                   style={{
                     position: 'absolute',
@@ -28,19 +28,19 @@ const Hero = () => {
                     width: '100%',
                     height: '100%',
                   }}
-                  title='2023-01-04 13-35-45.mkv'
+                  title="2023-01-04 13-35-45.mkv"
                 ></iframe>
               </div>
             </>
           )}
         </div>
 
-        <main className='hero-main'>
+        <main className="hero-main">
           <ul>
             <li>
               <FontAwesomeIcon
                 icon={solid('check')}
-                size='1x'
+                size="1x"
                 style={{ paddingRight: 15 }}
               />
               For POLISH Language Students
@@ -48,7 +48,7 @@ const Hero = () => {
             <li>
               <FontAwesomeIcon
                 icon={solid('check')}
-                size='1x'
+                size="1x"
                 style={{ paddingRight: 15 }}
               />
               Beginner to infinity and beyond
@@ -56,7 +56,7 @@ const Hero = () => {
             <li>
               <FontAwesomeIcon
                 icon={solid('check')}
-                size='1x'
+                size="1x"
                 style={{ paddingRight: 15 }}
               />
               Add your own custom content
@@ -64,7 +64,7 @@ const Hero = () => {
             <li>
               <FontAwesomeIcon
                 icon={solid('check')}
-                size='1x'
+                size="1x"
                 style={{ paddingRight: 15 }}
               />
               Use imagery to retain vocabulary
@@ -72,7 +72,7 @@ const Hero = () => {
             <li>
               <FontAwesomeIcon
                 icon={solid('check')}
-                size='1x'
+                size="1x"
                 style={{ paddingRight: 15 }}
               />
               Discover patterns of Conjugation
@@ -80,7 +80,7 @@ const Hero = () => {
             <li>
               <FontAwesomeIcon
                 icon={solid('check')}
-                size='1x'
+                size="1x"
                 style={{ paddingRight: 15 }}
               />
               Create custom flashcards
@@ -88,7 +88,7 @@ const Hero = () => {
             <li>
               <FontAwesomeIcon
                 icon={solid('check')}
-                size='1x'
+                size="1x"
                 style={{ paddingRight: 15 }}
               />
               All in a fully responsive Application
